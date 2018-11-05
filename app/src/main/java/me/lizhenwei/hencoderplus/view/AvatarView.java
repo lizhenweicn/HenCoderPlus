890c141d70493ac0ab4529aa461a862538ccaf89
lizhenwei <lizhenwei.will@foxmail.com>
2018-11-05 15:04:22 +0800

    [头像]控件绘制完成

--- app/src/main/java/me/lizhenwei/hencoderplus/view/AvatarView.java
+++ app/src/main/java/me/lizhenwei/hencoderplus/view/AvatarView.java
@@ -5,6 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.graphics.Xfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,11 +29,25 @@ public class AvatarView extends View {
     private static final int AVATAR_WIDTH = (int) Utils.dp2px(300);
 
     /**
+     * 图片显示的偏移量
+     */
+    private static final float PADDING = Utils.dp2px(50);
+
+    /**
+     * 头像的黑边框厚度
+     */
+    private static final float EDGE_PADDING = Utils.dp2px(10);
+
+    /**
      * 抗锯齿画笔
      */
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+    Xfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
+
     private Bitmap mBitmap;
+
+    private RectF savedArea = new RectF();
 
     public AvatarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -40,13 +58,24 @@ public class AvatarView extends View {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+        savedArea.set(PADDING, PADDING, PADDING + AVATAR_WIDTH, PADDING + AVATAR_WIDTH);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(mBitmap,
-                (getWidth() - mBitmap.getWidth()) / 2,
-                (getHeight() - mBitmap.getHeight()) / 2, mPaint);
+        canvas.drawOval(PADDING, PADDING, PADDING + AVATAR_WIDTH, PADDING + AVATAR_WIDTH, mPaint);
 
+        int saved = canvas.saveLayer(savedArea, mPaint);
+        canvas.drawOval(PADDING + EDGE_PADDING, PADDING + EDGE_PADDING,
+                PADDING + AVATAR_WIDTH - EDGE_PADDING, PADDING + AVATAR_WIDTH - EDGE_PADDING, mPaint);
+        mPaint.setXfermode(xfermode);
+        canvas.drawBitmap(mBitmap, PADDING, PADDING, mPaint);
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(saved);
     }
 
     private Bitmap getAvatar(int width) {
