6c03101055452fbcfc2e91e5aa91ef122de98c1a
lizhenwei <lizhenwei.will@foxmail.com>
2018-11-06 08:05:49 +0800

    [文字]单行文字绘制完成

--- app/src/main/java/me/lizhenwei/hencoderplus/view/SportsView.java
+++ app/src/main/java/me/lizhenwei/hencoderplus/view/SportsView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import me.lizhenwei.hencoderplus.utils.Utils;
@@ -17,6 +19,11 @@ import me.lizhenwei.hencoderplus.utils.Utils;
  * Author : will
  */
 public class SportsView extends View {
+
+    /**
+     * 需要绘制的文字信息
+     */
+    private static final String TEXT = "abab";
 
     /**
      * 环的宽度
@@ -43,14 +50,26 @@ public class SportsView extends View {
      */
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+    /*
+     * 静态文字 ：画笔绘制文字区域的左上右下
+     */
+//    Rect mRect = new Rect();
+
+    /**
+     * 动态文字
+     */
+    Paint.FontMetrics mFontMetrics = new Paint.FontMetrics();
+
     public SportsView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
     {
-//        mPaint.setTextSize(Utils.dp2px(100));
-//        mPaint.setTypeface(Typeface.createFromAsset(getContext()));
-//        mPaint.setTextAlign(Paint.Align.CENTER);
+        mPaint.setTextSize(Utils.dp2px(100));
+        mPaint.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "Quicksand-Regular.ttf"));
+        mPaint.setTextAlign(Paint.Align.CENTER);
+
+        mPaint.getFontMetrics(mFontMetrics);
     }
 
     @Override
@@ -72,8 +91,16 @@ public class SportsView extends View {
                 -90, 225, false, mPaint);
 
         //  绘制文字
-//        mPaint.setStyle(Paint.Style.FILL);
-//        canvas.drawText("abab", getWidth() / 2, getHeight() / 2, mPaint);
+        mPaint.setStyle(Paint.Style.FILL);
 
+        //  静态文字绘制方法
+//        mPaint.getTextBounds(TEXT, 0, TEXT.length(), rect);
+//        float offset = (mRect.top + mRect.bottom) / 2;
+
+        //  动态文字绘制方法
+//        mPaint.getFontMetrics(mFontMetrics);
+        float offset = (mFontMetrics.ascent + mFontMetrics.descent) / 2;
+
+        canvas.drawText(TEXT, getWidth() / 2, getHeight() / 2 - offset, mPaint);
     }
 }
